f97dd72fca9c2b1de8ea8cf1a442ea79b53a5e32
adminnz@gmail.com <adminnz@gmail.com>
2014-02-03 12:04:43 +1300

    a Simple chat client.

---
+++ ChatClient/ChatForm.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace ChatClient
+{
+    partial class ChatForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 38);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(481, 225);
+            this.listBox1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 271);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(481, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(56, 6);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(158, 20);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "Anonymous";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Name:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(505, 303);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ChatClient/ChatForm.cs
@@ -0,0 +1,74 @@
+﻿using Apache.NMS;
+using Apache.NMS.ActiveMQ;
+using Apache.NMS.ActiveMQ.Commands;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChatClient
+{
+    public partial class ChatForm : Form
+    {
+        IConnection connection;
+        ISession session;
+        IMessageConsumer consumer;
+        IMessageProducer producer;
+        public ChatForm()
+        {
+            InitializeComponent();
+            var factory = new ConnectionFactory("failover:(tcp://localhost:61616)");
+            connection = factory.CreateConnection();
+            connection.Start();
+            session = connection.CreateSession();
+            IDestination chatTopic = new ActiveMQTopic("ChatTopic");
+            producer = session.CreateProducer(chatTopic);
+            consumer = session.CreateConsumer(chatTopic);
+            consumer.Listener += consumer_Listener;
+        }
+
+
+        void consumer_Listener(IMessage message)
+        {
+            ITextMessage chatMessage = (ITextMessage)message;
+            string name = chatMessage.Properties.GetString("Name");
+            DisplayMessage(name, chatMessage.Text);
+        }
+
+        delegate void DisplayMessageCallback(string username, string message);
+        private void DisplayMessage(string username, string message)
+        {
+            if (this.listBox1.InvokeRequired)
+            {
+                DisplayMessageCallback d = new DisplayMessageCallback(DisplayMessage);
+                this.Invoke(d, new object[] { username, message });
+            }
+            else
+            {
+                this.listBox1.Items.Add(String.Format("{0}: {1}", username, message));
+            }
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                SendMessage();
+                e.Handled = true;
+            }
+        }
+
+        private void SendMessage()
+        {
+            ITextMessage chatMessage = session.CreateTextMessage(textBox1.Text);
+            chatMessage.Properties.SetString("Name", textBox2.Text);
+            producer.Send(chatMessage);
+            textBox1.Clear();
+        }
+    }
+}
